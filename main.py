20af970c1ab05344b9b31319c02473e28bd3c16b
GrimdarkChaos <randy@lennonverse.com>
2025-11-05 07:49:14 -0500

    Set pygame initialization and basic game loop

--- main.py
+++ main.py
@@ -11,7 +11,23 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+   
+    # Initializing Pygame
+    pygame.init()
 
+    # Set up screen dimensions
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # Game variables
+    running = True
+
+    # Game Loop
+    while running:
+        for event in pygame.event.get():    # This will check if the the user has
+            if event.type == pygame.QUIT:   # closed the window and close the game
+                return
+        screen.fill((0, 0, 0)) # Fill the Screen with Black
+        pygame.display.flip()  # Update the full display Surface to the screen
 
 if __name__ == "__main__":
     main()
