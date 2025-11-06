735a212dbe3c590424ed4d094fe59d0007a770f8
GrimdarkChaos <randy@lennonverse.com>
2025-11-06 14:56:25 -0500

    Slowed down shooting

--- player.py
+++ player.py
@@ -15,6 +15,7 @@ class Player(CircleShape):
         super().__init__(x, y, PLAYER_RADIUS)
         self.radius = PLAYER_RADIUS
         self.rotation = 0
+        self.timer = 0
     
     # in the player class
     def triangle(self):
@@ -33,6 +34,8 @@ class Player(CircleShape):
         pygame.draw.polygon(screen, "white", self.triangle(),2)
 
     def update(self, dt):
+        self.timer -= dt
+
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
@@ -55,5 +58,9 @@ class Player(CircleShape):
         self.position += forward * PLAYER_SPEED * dt
 
     def shoot(self, PLAYER_SHOOT_SPEED):
-        shot = Shot(self.position.x, self.position.y, SHOT_RADIUS)
-        shot.velocity = (pygame.Vector2(0, 1).rotate(self.rotation)) * PLAYER_SHOOT_SPEED
+        if self.timer > 0:
+            pass
+        else:
+            shot = Shot(self.position.x, self.position.y, SHOT_RADIUS)
+            shot.velocity = (pygame.Vector2(0, 1).rotate(self.rotation)) * PLAYER_SHOOT_SPEED
+            self.timer = PLAYER_SHOOT_COOLDOWN
